d0f2090290905f9c5be6aa1a356d86d7cb8bcbad
Mikhail Shardin <kriptonit1988@gmail.com>
2018-12-06 18:21:03 +0500

    Реализовал валидацию имени персонажа

--- js/setup.js
+++ js/setup.js
@@ -152,3 +152,25 @@ userNameInput.addEventListener('focus', function () {
 userNameInput.addEventListener('blur', function () {
   document.addEventListener('keydown', documentEscPressHandler);
 });
+
+userNameInput.addEventListener('invalid', function () {
+  if (userNameInput.validity.tooShort) {
+    userNameInput.setCustomValidity('Имя должно состоять минимум из 2-х символов');
+  } else if (userNameInput.validity.tooLong) {
+    userNameInput.setCustomValidity('Имя не должно превышать 25-ти символов');
+  } else if (userNameInput.validity.valueMissing) {
+    userNameInput.setCustomValidity('Обязательное поле');
+  } else {
+    userNameInput.setCustomValidity('');
+  }
+});
+
+userNameInput.addEventListener('input', function (evt) {
+  evt.preventDefault();
+  if (userNameInput.value.length < 2) {
+    userNameInput.setCustomValidity('Имя должно состоять минимум из 2-х символов');
+  } else {
+    userNameInput.setCustomValidity('');
+  }
+});
+
